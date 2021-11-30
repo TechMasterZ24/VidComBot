7c2608bf7396e427fcd90f04e46da8f707049c77
TechMasterZ24 <94572559+TechMasterZ24@users.noreply.github.com>
2021-11-30 10:14:22 +0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+from sample_config import Config
+
+class Development(Config):
+  APP_ID = 2135958158
+  API_HASH = "1ebf12e67222683abe1c01f0209b79df"
+  TG_BOT_TOKEN = "2135958158:AAGC1SbbROpYpT_wAtXLBNyzjKBqLQPxYrg"
+  AUTH_USERS = [
+    1412909688
+  ]
